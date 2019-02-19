833c66cf74e613b042493dc834404d1c30742b24
Carlos Bertomeu <carlos.bertomeu01@estudiant.upf.edu>
2019-02-19 12:15:22 +0100

    add items to table

--- dynamo-app/src/main/java/upf/dynamodb/app/AmazonDynamoDBSample.java
+++ dynamo-app/src/main/java/upf/dynamodb/app/AmazonDynamoDBSample.java
@@ -99,12 +99,12 @@ public class AmazonDynamoDBSample {
         init();
 
         try {
-            String tableName = "my-favorite-movies-table";
+            String tableName = "Seminar4";
 
             // Create a table with a primary hash key named 'name', which holds a string
             CreateTableRequest createTableRequest = new CreateTableRequest().withTableName(tableName)
-                .withKeySchema(new KeySchemaElement().withAttributeName("name").withKeyType(KeyType.HASH))
-                .withAttributeDefinitions(new AttributeDefinition().withAttributeName("name").withAttributeType(ScalarAttributeType.S))
+                .withKeySchema(new KeySchemaElement().withAttributeName("Nia").withKeyType(KeyType.HASH))
+                .withAttributeDefinitions(new AttributeDefinition().withAttributeName("Nia").withAttributeType(ScalarAttributeType.S))
                 .withProvisionedThroughput(new ProvisionedThroughput().withReadCapacityUnits(1L).withWriteCapacityUnits(1L));
 
             // Create table if it does not exist yet
@@ -118,23 +118,29 @@ public class AmazonDynamoDBSample {
             System.out.println("Table Description: " + tableDescription);
 
             // Add an item
-            Map<String, AttributeValue> item = newItem("Bill & Ted's Excellent Adventure", 1989, "****", "James", "Sara");
+            Map<String, AttributeValue> item = newItem(183768,"Carlos", "LSDS");
             PutItemRequest putItemRequest = new PutItemRequest(tableName, item);
             PutItemResult putItemResult = dynamoDB.putItem(putItemRequest);
             System.out.println("Result: " + putItemResult);
 
             // Add another item
-            item = newItem("Airplane", 1980, "*****", "James", "Billy Bob");
+            item = newItem(18888,"Jose", "ECV");
             putItemRequest = new PutItemRequest(tableName, item);
             putItemResult = dynamoDB.putItem(putItemRequest);
             System.out.println("Result: " + putItemResult);
 
-            // Scan items for movies with a year attribute greater than 1985
+            // Add another item
+            item = newItem(19999,"Marcel", "DA");
+            putItemRequest = new PutItemRequest(tableName, item);
+            putItemResult = dynamoDB.putItem(putItemRequest);
+            System.out.println("Result: " + putItemResult);
+
+            // Scan
             HashMap<String, Condition> scanFilter = new HashMap<String, Condition>();
             Condition condition = new Condition()
-                .withComparisonOperator(ComparisonOperator.GT.toString())
-                .withAttributeValueList(new AttributeValue().withN("1985"));
-            scanFilter.put("year", condition);
+                .withComparisonOperator(ComparisonOperator.EQ.toString())
+                .withAttributeValueList(new AttributeValue("18888"));
+            scanFilter.put("Nia", condition);
             ScanRequest scanRequest = new ScanRequest(tableName).withScanFilter(scanFilter);
             ScanResult scanResult = dynamoDB.scan(scanRequest);
             System.out.println("Result: " + scanResult);
@@ -155,12 +161,11 @@ public class AmazonDynamoDBSample {
         }
     }
 
-    private static Map<String, AttributeValue> newItem(String name, int year, String rating, String... fans) {
+    private static Map<String, AttributeValue> newItem(long nia, String name, String subject) {
         Map<String, AttributeValue> item = new HashMap<String, AttributeValue>();
-        item.put("name", new AttributeValue(name));
-        item.put("year", new AttributeValue().withN(Integer.toString(year)));
-        item.put("rating", new AttributeValue(rating));
-        item.put("fans", new AttributeValue().withSS(fans));
+        item.put("Nia", new AttributeValue(Long.toString(nia)));
+        item.put("Name", new AttributeValue(name));
+        item.put("Subject", new AttributeValue(subject));
 
         return item;
     }
