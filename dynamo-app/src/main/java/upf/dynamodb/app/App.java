981e0255e9d20b91d0f8d6e7f6ab2bd7d799e3d0
Carlos Bertomeu <carlos.bertomeu01@estudiant.upf.edu>
2019-02-19 11:36:02 +0100

    initial project

---
+++ dynamo-app/src/main/java/upf/dynamodb/app/App.java
@@ -0,0 +1,14 @@
+package upf.dynamodb.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
